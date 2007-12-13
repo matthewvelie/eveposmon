854b856f9d2777a740943b74fd0b7c5910b0d428
lewisvance <lewisvance@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2007-12-13 08:00:47 +0000

    added data dir for xml

---
+++ TowerResources.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace EVEPOSMon
+{
+    class TowerResources
+    {
+    }
+}
