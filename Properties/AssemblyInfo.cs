d79849df7a0b5c513e19f5f5fde23aa33de74aa9
matthewvelie <matthewvelie@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2007-12-22 02:40:47 +0000

    Upped to 1.0.0.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
