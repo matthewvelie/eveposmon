f6a2204f231255f47b520f82d4d2f1f7cba517b9
matthewvelie <matthewvelie@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2007-12-22 02:22:00 +0000

    made a copy

---
+++ tags/1002/CharacterSelect.Designer.cs
@@ -0,0 +1,80 @@
+namespace EVEPOSMon
+{
+    partial class CharacterSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCharacters = new System.Windows.Forms.ListBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbCharacters
+            // 
+            this.lbCharacters.FormattingEnabled = true;
+            this.lbCharacters.Location = new System.Drawing.Point(13, 13);
+            this.lbCharacters.Name = "lbCharacters";
+            this.lbCharacters.Size = new System.Drawing.Size(203, 69);
+            this.lbCharacters.TabIndex = 0;
+            this.lbCharacters.DoubleClick += new System.EventHandler(this.lbCharacters_DoubleClick);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSelect.Location = new System.Drawing.Point(66, 88);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // CharacterSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 121);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lbCharacters);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CharacterSelect";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CharacterSelect";
+            this.Load += new System.EventHandler(this.CharacterSelect_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbCharacters;
+        private System.Windows.Forms.Button btnSelect;
+    }
+}
---
+++ tags/1002/SelectStarbases.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+using EVEMon.Common;
+
+
+namespace EVEPOSMon
+{
+    public partial class SelectStarbases : Form
+    {
+        Settings m_settings = Settings.GetInstance();
+        private MainScreen mainScreen;
+        
+        public SelectStarbases(MainScreen screen)
+        {
+            InitializeComponent();
+            this.mainScreen = screen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            addAvailableStarbasesToDataGridView();
+            updateStarbaseListTimes();
+        }
+
+        private void updateStarbaseListTimes()
+        {
+            if (m_settings.starbaseList.cachedUntil == DateTime.MinValue)
+            {
+                tsslCachedUntil.Text = "Unknown";
+            }
+            else
+            {
+                tsslCachedUntil.Text = m_settings.starbaseList.cachedUntil.ToLocalTime().ToString();
+            }
+        }
+
+        private void btnLoadStations_Click(object sender, EventArgs e)
+        {
+            btnLoadStations.Enabled = false; 
+            
+            dgStations.Rows.Clear();
+            Starbase.LoadStarbaseListFromApi();
+            addAvailableStarbasesToDataGridView();
+            updateStarbaseListTimes();
+
+            btnLoadStations.Enabled = true;
+        }
+
+        public void addAvailableStarbasesToDataGridView()
+        {
+            dgStations.Rows.Clear();
+            foreach (Starbase s in m_settings.availableStarBases)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dgStations, new object[] { s.monitored, s.StarbaseSystem.regionName, s.StarbaseSystem.constellationName, s.Moon.moonName});
+                row.Tag = s;
+                dgStations.Rows.Add(row);
+            }
+        }
+
+        private void populateTabs()
+        {
+            int count = 0;
+            foreach (DataGridViewRow row in dgStations.Rows)
+            {
+                Starbase starbase = row.Tag as Starbase;
+                if (starbase.monitored == true)
+                    count++;
+            }
+            if (count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Please select at least one Starbase to monitor");
+                return;
+            }
+
+            mainScreen.updateTabs();
+        }
+
+        private void btnGetStationInfo_Click(object sender, EventArgs e)
+        {
+            populateTabs();
+
+            this.Visible = false;
+            mainScreen.Focus();
+        }
+
+        private void SelectStarbases_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Starbase.SerializeStarbasesToFile(m_settings.SerializedStarbasesFilename, m_settings.availableStarBases);
+        }
+
+        private void btnSaveAutoload_Click(object sender, EventArgs e)
+        {
+            Starbase.SerializeStarbasesToFile(m_settings.SerializedStarbasesFilename, m_settings.availableStarBases);
+        }
+
+        private void characterInformationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoginCharacterSelect lcs = new LoginCharacterSelect();
+        }
+
+        private void dgStations_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+
+            DataGridViewRow row = dgStations.Rows[e.RowIndex];
+            Starbase s = row.Tag as Starbase;
+            if (Convert.ToBoolean(row.Cells[0].Value) == true)
+            {
+                s.monitored = true;
+            }
+            else
+            {
+                s.monitored = false;
+            }
+        }
+    }
+}
