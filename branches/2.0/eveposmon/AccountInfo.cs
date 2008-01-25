a4f35968609091b351f099f564abb811c24f3381
lewisvance <lewisvance@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2008-01-25 08:06:45 +0000

    Added AccountInfo.cs

---
+++ branches/2.0/eveposmon/AccountInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace eveposmon
+{
+    public partial class AccountInfo : Form
+    {
+        public AccountInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
