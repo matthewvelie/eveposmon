0b4afc86d5c9ba09298a1002a29e9f150f4f355b
lewisvance <lewisvance@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2008-01-25 05:16:14 +0000

    added TowerResources

--- branches/andy/UnitTests/StaticDataTests.cs
+++ branches/andy/UnitTests/StaticDataTests.cs
@@ -26,5 +26,51 @@ namespace UnitTests
             Constellations.LoadFromFile(@"..\..\..\data\Constellations.xml.gz");
             Assert.AreEqual(null, Constellations.GetConstellationById(0));
         }
+
+        [Test]
+        public void TowerResourcesTest()
+        {
+            TowerResources.LoadFromFile(@"..\..\..\data\controlTowerResources.xml.gz");
+
+            TowerResources.Tower tower = TowerResources.GetByTypeId(12235);
+
+            Assert.AreEqual(null, TowerResources.GetByTypeId(0));
+
+            TowerResources.Fuel fuel = tower.FuelList[0];
+            Assert.AreEqual(44, fuel.TypeId);
+            Assert.AreEqual(1, fuel.purpose);
+            Assert.AreEqual(4, fuel.Quantity);
+            Assert.AreEqual(-1, fuel.Minsec);
+            Assert.AreEqual(-1, fuel.faction);
+
+            // fuel is required in all cases
+            Assert.IsTrue(tower.RequiresFuel(44, 0.1, 500003));
+            Assert.IsTrue(tower.RequiresFuel(44, 0.5, 500003));
+            Assert.IsTrue(tower.RequiresFuel(44, 0.5, 500002));
+            Assert.IsTrue(tower.RequiresFuel(44, 0.1, 500002));
+
+            fuel = tower.FuelList[9];
+            Assert.AreEqual(24592, fuel.TypeId);
+            Assert.AreEqual(1, fuel.purpose);
+            Assert.AreEqual(1, fuel.Quantity);
+            Assert.AreEqual(0.4, fuel.Minsec);
+            Assert.AreEqual(500003, fuel.faction);
+
+            // faction matches and
+            // security status below minsec so fuel not required
+            Assert.IsFalse(tower.RequiresFuel(24592, 0.1, 500003));
+
+            // faction matches and
+            // security status above minsec so required
+            Assert.IsTrue(tower.RequiresFuel(24592, 0.5, 500003));
+
+            // security status above minsec and
+            // faction doesn't match so fuel not required
+            Assert.IsFalse(tower.RequiresFuel(24592, 0.5, 500002));
+
+            // security status below minsec and
+            // faction doesn't match so not required
+            Assert.IsFalse(tower.RequiresFuel(24592, 0.1, 500002));
+        }
     }
 }
