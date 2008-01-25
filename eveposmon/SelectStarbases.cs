ac5179a52c400f520b8ff7d899a30eefbce6ac79
lewisvance <lewisvance@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2008-01-25 08:12:24 +0000

    Added SelectStarbases.cs

---
+++ eveposmon/SelectStarbases.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+
+namespace eveposmon
+{
+    public partial class SelectStarbases : Form
+    {
+        public SelectStarbases(MainScreen screen)
+        {
+            InitializeComponent();
+        }
+    }
+}
