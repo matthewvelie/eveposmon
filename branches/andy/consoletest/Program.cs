7af090116f12c833df1a2aae686ac18f66387c26
lewisvance <lewisvance@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2008-01-25 05:53:33 +0000

    added Towers

--- branches/andy/consoletest/Program.cs
+++ branches/andy/consoletest/Program.cs
@@ -10,7 +10,9 @@ namespace consoletest
     {
         static void Main(string[] args)
         {
-            TowerResources.LoadFromFile(@"..\..\..\data\controlTowerResources.xml.gz");
+            Towers.LoadFromFile(@"..\..\..\data\invControlTowers.xml.gz");
+            Towers.Tower tower = Towers.GetTowerByTypeId(12235);
+            Console.WriteLine(tower.Description);
         }
     }
 }
