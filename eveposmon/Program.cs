2b803f3336fb60b5efbfbe51db2051d5d5e05f1d
lewisvance <lewisvance@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2008-01-25 08:24:58 +0000

    added static data loading

--- eveposmon/Program.cs
+++ eveposmon/Program.cs
@@ -12,6 +12,25 @@ namespace eveposmon
         [STAThread]
         static void Main()
         {
+            try
+            {
+                StaticData.Constellations.LoadFromFile("data/constellations.xml.gz");
+                StaticData.TowerResources.LoadFromFile("data/controlTowerResources.xml.gz");
+                StaticData.Factions.LoadFromFile("data/factions.xml.gz");
+                StaticData.Towers.LoadFromFile("data/invControlTowers.xml.gz");
+                StaticData.Fuels.LoadFromFile("data/invFuelTypes.xml.gz");
+                StaticData.Moons.LoadFromFile("data/moonData.xml.gz");
+                StaticData.Regions.LoadFromFile("data/regions.xml.gz");
+                StaticData.SolarSystems.LoadFromFile("data/solarSystem.xml.gz");
+                StaticData.SystemFactions.LoadFromFile("data/solarSystemFactions.xml.gz");
+                StaticData.Stations.LoadFromFile("data/staStations.xml.gz");
+            }
+            catch
+            {
+                MessageBox.Show("Error loading static data, exiting");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainScreen());
