562a8bc98020ad14fa4d98a99504863a78f1206f
lewisvance@gmail.com <lewisvance@gmail.com@aed27bfc-2241-0410-8320-43ff41d6c5f2>
2008-01-26 07:37:44 +0000

    added AccountAdded event

--- branches/2.0/eveposmon/AccountInfo.cs
+++ branches/2.0/eveposmon/AccountInfo.cs
@@ -12,11 +12,11 @@ using libeveapi;
 
 namespace eveposmon
 {
+    public delegate void AccountAddedEventHandler(object sender, AccountInfo.AccountAddedEventArgs e);
+
     public partial class AccountInfo : Form
     {
-        private int userId;
-        private string apiKey;
-        private CharacterList.CharacterListItem characterListItem;
+        public event AccountAddedEventHandler AccountAdded;
 
         public AccountInfo()
         {
@@ -32,38 +32,21 @@ namespace eveposmon
 
             try
             {
-                userId = Convert.ToInt32(tbUserId.Text);
+                int userId = Convert.ToInt32(tbUserId.Text);
+                CharacterList characterList = EveApi.GetAccountCharacters(userId, tbApiKey.Text);
+                CharacterSelect characterSelect = new CharacterSelect(characterList);
+
+                if (characterSelect.ShowDialog() == DialogResult.OK)
+                {
+                    tbCharacterName.Text = characterSelect.SelectedCharacterListItem.Name;
+                    tbCharacterName.Tag = characterSelect.SelectedCharacterListItem;
+                    btnAddAccount.Enabled = true;
+                }
             }
             catch (System.FormatException)
             {
                 return;
             }
-
-            CharacterList characterList = EveApi.GetAccountCharacters(userId, tbApiKey.Text);
-            CharacterSelect characterSelect = new CharacterSelect(characterList);
-
-            if (characterSelect.ShowDialog() == DialogResult.OK)
-            {
-                tbCharacterName.Text = characterSelect.SelectedCharacterListItem.Name;
-                this.apiKey = tbApiKey.Text;
-                this.characterListItem = characterSelect.SelectedCharacterListItem;
-                btnAddAccount.Enabled = true;
-            }
-        }
-
-        public int UserId
-        {
-            get { return userId; }
-        }
-
-        public string ApiKey
-        {
-            get { return apiKey; }
-        }
-
-        public CharacterList.CharacterListItem CharacterListItem
-        {
-            get { return characterListItem; }
         }
 
         private void tbUserId_TextChanged(object sender, EventArgs e)
@@ -92,9 +75,39 @@ namespace eveposmon
 
         private void btnAddAccount_Click(object sender, EventArgs e)
         {
-
-            btnAddAccount.Enabled = false;
+            try
+            {
+                int userId = Convert.ToInt32(tbUserId.Text);
+                string apiKey = tbApiKey.Text;
+                CharacterList.CharacterListItem characterListItem = tbCharacterName.Tag as CharacterList.CharacterListItem;
+                OnAccountAdded(new AccountAddedEventArgs(userId, apiKey, characterListItem));
+                btnAddAccount.Enabled = false;
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Please enter a valid UserId");
+            }
         }
 
+        public class AccountAddedEventArgs : EventArgs
+        {
+            public Settings.Account Account;
+
+            public AccountAddedEventArgs(int userId, string apiKey, CharacterList.CharacterListItem characterListItem)
+            {
+                this.Account = new Settings.Account();
+                Account.UserId = userId;
+                Account.ApiKey = apiKey;
+                Account.CharacterListItem = characterListItem;
+            }
+        }
+
+        protected virtual void OnAccountAdded(AccountAddedEventArgs e)
+        {
+            if (this.AccountAdded != null)
+            {
+                AccountAdded(this, e);
+            }
+        }
     }
 }
